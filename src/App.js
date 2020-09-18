4c492648c60b9e56e63e0d1a8be17d475c3771bf
ssma100701 <ssma100701@gmail.com>
2020-09-19 01:17:54 +1000

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,61 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import './App.css';
+import Todo from './Todo';
+
+const initialState = [];
+
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo',
+};
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)];
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete };
+        }
+        return todo;
+      });
+    case ACTIONS.DELETE_TODO:
+      return todos.filter((todo) => todo.id !== action.payload.id);
+    default:
+      return todos;
+  }
+}
+
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false };
+}
 
 function App() {
-  return <div className='App'>React App</div>;
+  const [todos, dispatch] = useReducer(reducer, initialState);
+  const [name, setName] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    setName('');
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
+      {todos.map((todo) => (
+        <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
