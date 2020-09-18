0fd49b189834fa77ae01e84c7cde04524dd0ca93
ssma100701 <ssma100701@gmail.com>
2020-09-18 22:34:19 +1000

    useRef

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'>React App</div>;
+  const [name, setName] = useState('');
+  const prevName = useRef('');
+
+  useEffect(() => {
+    prevName.current = name;
+  }, [name]);
+
+  return (
+    <div>
+      <input
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <div>
+        My name is {name} and it used to be {prevName.current}
+      </div>
+    </div>
+  );
 }
 
 export default App;
